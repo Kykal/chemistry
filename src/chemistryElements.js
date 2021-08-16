5f3e19009ac3983cbfe696318da978cd6f3f88bd
Kykal <benavides.alan@hotmail.com>
2021-08-16 10:59:40 -0500

    Added args

--- src/chemistryElements.js
+++ src/chemistryElements.js
@@ -1,11 +1,8 @@
-import { element } from "./chemistryConstructor";
+import { Element } from "./chemistryConstructor";
 
-const hydrogen = new element( 'gas', 1.0080, '1s1', '+1, -1', 2.2, 13.598, 0.754, 13.81, 20.28, 0.00008988, 1766 );
-const helium = new element( 'noble gas', 4.00260, '1s2', 0, null, 140, 24.587, null, 0.95, 4.22, 0.0001785, 1868 )
+const hydrogen = new Element( 'Hydrogen', 'H', 1, 'Gas', 1.0080, '1s1', '+1, -1', 2.3, 2.2, 120, 25, '31(5)', 13.598, 0.754, 13.81, 20.28, 0.00008988, 1766, 'Henry Cavendish', 'Non Metals', 1, 'IA', 1 );
 
-const elements = [
-   hydrogen,
-   helium
-];
 
-module.exports = { elements };
+module.exports = {
+   hydrogen
+}
